ed880c35b3451b67ed459278fa7daf4d574c6aa0
BEKETOV <s-ebeketov@lwsd.org>
2023-10-31 08:48:20 -0700

    Added price edgecase fix for reals this time

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ async function logItems() {
         'Accept': 'application/json',
     },
   })
-  
+
   const json = await response.json()
   return json
 }
@@ -30,13 +30,15 @@ function openCase(weaponsObject) {
     document.getElementById("name").style.color = `#${randomWeapon.rarity_color}`;
     document.getElementById("randomskindiv").style.borderColor = `#${randomWeapon.rarity_color}`;
 
-    if (randomWeapon.price.hasOwnProperty("7_days") && randomWeapon.price["7_days"].average != 0) {
-      document.getElementById("price").innerHTML = randomWeapon.price["7_days"].average + "$"
-    } else if (randomWeapon.price.hasOwnProperty("30_days") && randomWeapon.price["30_days"].average != 0) {
-      document.getElementById("price").innerHTML = randomWeapon.price["30_days"].average + "$"
-    } else if (randomWeapon.price.hasOwnProperty("all_time") && randomWeapon.price["all_time"].average != 0) {
-      document.getElementById("price").innerHTML = randomWeapon.price["all_time"].average + "$"
-    } else {
+    try {
+      if (randomWeapon.price.hasOwnProperty("7_days") && randomWeapon.price["7_days"].average != 0) {
+        document.getElementById("price").innerHTML = randomWeapon.price["7_days"].average + "$"
+      } else if (randomWeapon.price.hasOwnProperty("30_days") && randomWeapon.price["30_days"].average != 0) {
+        document.getElementById("price").innerHTML = randomWeapon.price["30_days"].average + "$"
+      } else if (randomWeapon.price.hasOwnProperty("all_time") && randomWeapon.price["all_time"].average != 0) {
+        document.getElementById("price").innerHTML = randomWeapon.price["all_time"].average + "$"
+      }
+    } catch {
       document.getElementById("price").innerHTML = "No price to display"
     }
   };
@@ -64,9 +66,28 @@ async function main() {
       const type = item.type;
 
       // Check if the value of the `"type"` key is `"Weapon"` and also make sure its not a deagle skin that doesnt exist
-      if (type === "Weapon" && item.tradable === 1 && item.icon_url !== "-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQh5hlcX0nvUOGsx8DdQBJjIAVHubSaLwJh1P_NP28b6YSwxITck6f2Nu7UwTgG7JF33L-S8Imtigft_xFpMGCmJIbHJlIgIQaH3GjvkKk") {
+      if ((type === "Weapon" || type === "Gloves") && item.tradable === 1 && item.icon_url !== "-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQh5hlcX0nvUOGsx8DdQBJjIAVHubSaLwJh1P_NP28b6YSwxITck6f2Nu7UwTgG7JF33L-S8Imtigft_xFpMGCmJIbHJlIgIQaH3GjvkKk") {
         // Add the weapon to the `weaponsObject` object, using its name as the key
+        
         weaponsObject[item.name] = item;
+
+        switch (item.rarity) {
+          case "Mil-Spec Grade":
+            //weaponsObject[item.name] = item;
+            break
+          case "Restricted":
+            //weaponsObject[item.name] = item;
+            break
+          case "Classified":
+            //weaponsObject[item.name] = item;
+            break
+          case "Covert":
+            //weaponsObject[item.name] = item;
+            break
+          case "Consumer Grade":  
+            //weaponsObject[item.name] = item;
+            break
+        }
       }
     }
   }
